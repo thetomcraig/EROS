1899dc51805b1d7d2154c01aaf9e17a000c12a1e
Tom Craig <thetomcaig@icloud.com>
2015-11-25 11:42:39 -0800

    Working on the bootstrap - wsgi problems

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+#application = DjangoWhiteNoise(application)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")

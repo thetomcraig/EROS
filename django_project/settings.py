92ba24f5b2b4227254c1491115b42a830dfdd101
Tom Craig <thetomcaig@icloud.com>
2016-02-12 21:18:25 -0800

    static file issue

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,13 +120,27 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+"""
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (('static/'),('scrapers/static/'),)
 STATIC_ROOT = '/static'
 LOGIN_REDIRECT_URL = '/'
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
-#Heroku
-DISABLE_COLLECTSTATIC=1
+"""
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+    )
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+
+
+   
+
+
 
 #Defaults to the HEROKU credentials
 SOCIAL_AUTH_REDIRECT_IS_HTTPS = True

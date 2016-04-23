b9f35403b7e86a4a651fdf31970fa471c06dd9d3
Tom Craig <thetomcraig@icloud.com>
2016-04-23 15:37:50 -0700

    fixed hostname issue

--- scrape_new_content.sh
+++ scrape_new_content.sh
@@ -1,6 +1,6 @@
 logs_path=${HOME}/Dropbox/TomCraig/Logs/EROS
 cd $logs_path
-file_name=${HOSTNAME}_database_output_$(date +"%y-%m-%d-%H-%M-%S").txt
+file_name=$(hostname)_database_output_$(date +"%y-%m-%d-%H-%M-%S").txt
 touch $file_name
 
 cd ${HOME}/Dropbox/TomCraig/Projects/EROS/

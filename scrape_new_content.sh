842d790fcd9f6ee397bcb3989807921b0e1d3a05
Tom <thetomcraig@icloud.com>
2016-04-22 19:13:23 -0700

    updated to faciliate running outside dropbox

--- scrape_new_content.sh
+++ scrape_new_content.sh
@@ -1,4 +1,4 @@
-cd ./logs
+cd ${HOME}/Dropbox/TomCraig/Logs/EROS
 
 file_name=${HOSTNAME}_database_output_$(date +"%F").txt
 touch $file_name
@@ -6,5 +6,5 @@ touch $file_name
 echo $(date) >> $file_name
 echo ","  >> $file_name
 
-#${HOME}/.virtualenvs/EROS/bin/python manage.py scrape twitter_users:all facebook_users:none >> $file_name
+${HOME}/.virtualenvs/EROS/bin/python manage.py scrape twitter_users:all facebook_users:none >> $file_name
 

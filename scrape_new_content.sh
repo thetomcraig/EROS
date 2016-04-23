729903021a1962e13b970aeee5236c23d64d02d2
Tom Craig <thetomcraig@icloud.com>
2016-04-22 21:30:17 -0700

    Fixed the management command to NOT use urllib

--- scrape_new_content.sh
+++ scrape_new_content.sh
@@ -2,7 +2,7 @@ logs_path=${HOME}/Dropbox/TomCraig/Logs/EROS
 cd $logs_path
 
 file_name=${HOSTNAME}_database_output_$(date +"%y-%m-%d-%H-%M-%S").txt
-if [[ ! $file_name ]]; then touch $file_name; fi
+touch $file_name
 
 cd ${HOME}/Dropbox/TomCraig/Projects/EROS/
 ${HOME}/.virtualenvs/EROS/bin/python manage.py scrape twitter_users:all facebook_users:none >> $logs_path/$file_name

08aa1bf251d5c36a4e96c68e5e0ff81fa643a678
Tom Craig <thetomcraig@icloud.com>
2016-04-23 18:20:56 -0700

    Added latest keyword to logs

--- scrape_new_content.sh
+++ scrape_new_content.sh
@@ -1,7 +1,14 @@
 logs_path=${HOME}/Dropbox/TomCraig/Logs/EROS
 cd $logs_path
-file_name=$(hostname)_database_output_$(date +"%y-%m-%d-%H-%M-%S").txt
+
+#Remove 'latest' from the last log file
+latest=$(ls *_latest*)
+mv -i $latest `echo $latest | sed -e 's/_latest_//gI' -e 's/_//gI' `
+
+#Make new log file
+file_name=$(hostname)_database_output_latest_$(date +"%y-%m-%d-%H-%M-%S").txt
 touch $file_name
 
+#Write to the file
 cd ${HOME}/Dropbox/TomCraig/Projects/EROS/
 ${HOME}/.virtualenvs/EROS/bin/python manage.py scrape --twitter_users existing >> $logs_path/$file_name

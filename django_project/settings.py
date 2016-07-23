1345e73b3a395abc188fa90079d194417f2bbd2a
Tom Craig <thetomcraig@icloud.com>
2016-07-23 16:39:22 -0700

    Working with the postges db connector

--- django_project/settings.py
+++ django_project/settings.py
@@ -95,13 +95,20 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+import dj_database_url
+in_heroku = False
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+if in_heroku:
+    DATABASES = {'default': dj_database_url.config()}
+else:
+  DATABASES = {
+      'default': {
+          'ENGINE': 'django.db.backends.sqlite3',
+          'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      }
+  }
 
 
 # Internationalization

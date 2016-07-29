b0bcd12c1dccc46d10bb46f0daece506153a58fe
Tom Craig <tom@TYCHO.local>
2016-07-28 19:16:23 -0700

    Sessions issue

--- django_project/settings.py
+++ django_project/settings.py
@@ -114,12 +114,6 @@ else:
       }
   }
 """
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Internationalization

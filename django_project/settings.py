e8a4d30b9d66ff843e9b01d71327fffbca9a7f5c
Tom Craig <tom@TYCHO.local>
2016-07-28 20:10:29 -0700

    static issue

--- django_project/settings.py
+++ django_project/settings.py
@@ -132,12 +132,11 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
-    os.path.join(PROJECT_ROOT, 'django_project/static'),
     )
-
 
 #Defaults to the HEROKU credentials
 SOCIAL_AUTH_REDIRECT_IS_HTTPS = True

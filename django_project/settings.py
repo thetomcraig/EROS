85eea63af6b324f69e5f8ed5e3280bf8bb4a3c8a
Tom Craig <thetomcraig@icloud.com>
2016-08-14 18:15:18 -0700

    Working on static files, so dropdown works

--- django_project/settings.py
+++ django_project/settings.py
@@ -135,12 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-    )
+  os.path.join(PROJECT_ROOT, 'static'),
+)
 
 #Defaults to the HEROKU credentials
 SOCIAL_AUTH_REDIRECT_IS_HTTPS = True

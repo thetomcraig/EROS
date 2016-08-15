c1be781be42322423216c247b0aaa6a295bd958f
Tom Craig <thetomcraig@icloud.com>
2016-08-14 18:20:12 -0700

    working on static assets issue

--- django_project/settings.py
+++ django_project/settings.py
@@ -140,6 +140,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
   os.path.join(PROJECT_ROOT, 'static'),
+  os.path.join(PROJECT_ROOT, '../static'),
 )
 
 #Defaults to the HEROKU credentials

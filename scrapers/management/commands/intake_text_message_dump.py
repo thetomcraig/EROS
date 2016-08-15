44354c04e2e007fd78e79536f86ba4470756cdf3
Tom Craig <thetomcraig@icloud.com>
2016-08-14 17:36:39 -0700

    working on issue with text message person objects

--- scrapers/management/commands/intake_text_message_dump.py
+++ scrapers/management/commands/intake_text_message_dump.py
@@ -15,11 +15,14 @@ class Command(BaseCommand):
 
   def handle(self, *args, **options):
     if options['path_to_iOSBackup']:
+      print len(TextMessagePerson.objects.all()) 
+      """
       if len(TextMessagePerson.objects.all()) == 0:
-        t = TextMessagePerson.objects.create(username="tomcraig", real_name="Tom Craig")
+        t = TextMessagePerson.objects.create(username='tomcraig', real_name='Tom Craig')
       else:
         [x.delete() for x in TextMessagePerson.objects.all()]
-        t = TextMessagePerson.objects.or_create(username="tomcraig", real_name="Tom Craig")
-      t.intake_raw_io_backup_texts(options['path_to_iOSBackup'] + "/_export")
-      t.save()
+        t = TextMessagePerson.objects.or_create(username='tomcraig', real_name='Tom Craig')
+      """
+      #t.intake_raw_io_backup_texts(options['path_to_iOSBackup'] + "/_export")
+      #t.save()
 

8b7929590754e4d0c94f1fa123664fcefdf1c909
Tom Craig <tom@TYCHO.local>
2016-08-20 09:31:24 -0700

    Consolidated

--- scrapers/management/commands/scrape.py
+++ scrapers/management/commands/scrape.py
@@ -7,11 +7,17 @@ from scrapers.models.twitter import TwitterPost
 from datetime import datetime
 from settings import OUTLETS
 
+command_arguments = { \
+  "--twitter": "top",
+  "--intake_iOSBackup_for_text_messages_at": "path",
+  "--intake_file_for_plain_text_at": "path" }
+
 usage = "python manage.py scrape "
 
 class Command(BaseCommand):
-  for arg in OUTLETS:
+  for arg in command_arguments:
     usage += arg 
+
 
   def add_arguments(self, parser):
     for arg in command_arguments:
@@ -19,16 +25,22 @@ class Command(BaseCommand):
 
 
   def handle(self, *args, **options):
-    if options['plain_text']:
-      pass
-
-    if options['text_message']:
-      pass
-
     if options['twitter']:
       tom = User.objects.get_or_create(username='tom')[0]
       tom.scrape_top_twitter_people()
       
-    if options['facebook']:
-      pass
+    if options['intake_iOSBackup_for_text_messages_at']:
+      u = User.objects.first()
+      t = TextMessagePerson.objects.get_or_create(user=u)[0]
+      t.intake_raw_io_backup_texts(options['intake_iOSBackup_for_text_messages_at'] + "/_export")
+      t.save()
 
+
+    if options['intake_file_for_plain_text_at']:
+			file_name = options['intake_file_for_plain_text_at']
+			sentences = utils.read_source_into_sentence_list(file_name)
+			author = LiteraturePerson.objects.get_or_create(username=options['author'], real_name=options['author'])[0]
+			for sentence in sentences: 
+				l = LiteratureSentence(author=author, content=" ".join(sentence))
+				l.save()
+				author.cache_sentence(l)

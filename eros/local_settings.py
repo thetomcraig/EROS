a533efa89c7e3c59b1db56e9eaccd381a0bd017b
Tom Craig <tcraig@tcraig-m01.ad.savagebeast.com>
2016-10-05 08:20:12 -0700

    localsettings

--- eros/local_settings.py
+++
@@ -1,3 +0,0 @@
-psql_pass = 'Zork69105'                                                                                                 
-instagram_username = 'thetomcraig@icloud.com'                                                                           
-instagram_password = 'fac3b00k' 

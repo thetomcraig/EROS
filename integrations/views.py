0129b2a51243bbb8e7d996ef3da74267a93cfbdb
Tom Craig <thetomcraig@icloud.com>
2016-10-05 21:13:42 -0700

    Working on instagram home

--- integrations/views.py
+++ integrations/views.py
@@ -1,18 +1,14 @@
-from django.views.generic.detail import DetailView
 from django.shortcuts import render_to_response
-from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
-from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
-from integrations.models.base import Person, Sentence, MarkovChain, SentenceCache
-from integrations.models.twitter import TwitterPerson, TwitterPost, TwitterPostMarkov
-from integrations.models.text_message import TextMessagePerson, TextMessage, TextMessageCache, TextMessageMarkov
+from integrations.models.twitter import TwitterPerson, TwitterPost
+from integrations.models.text_message import TextMessage, TextMessageCache, TextMessageMarkov
 from integrations.helpers.utils import (
-    scrape_twitter_person, 
+    scrape_twitter_person,
     apply_markov_chains_twitter,
     get_num_instagram_followers,
-    refresh_and_return_me_from_instagram,
     get_me_from_instagram,
     refresh_instagram_followers,
     follow_my_instagram_followers)
@@ -20,16 +16,17 @@ from integrations.helpers.utils import (
 import json
 import re
 
+
 def home(request):
     if(request.GET.get('text_message_home')):
         texts = TextMessage.objects.all()
         markov_texts = TextMessageMarkov.objects.all()
         first_words = TextMessageCache.objects.filter(beginning=True)
         first_words_no_dupes = set([x.word1 for x in first_words])
-        context = RequestContext(request,    \
-            {'request': request, \
-            'first_words': first_words_no_dupes, \
-            'texts': texts, \
+        context = RequestContext(request, {
+            'request': request,
+            'first_words': first_words_no_dupes,
+            'texts': texts,
             'markov_texts': markov_texts})
         return render_to_response('integrations/text_message_home.html', context_instance=context)
 
@@ -39,7 +36,7 @@ def home(request):
     if(request.GET.get('instagram_home')):
         return HttpResponseRedirect(reverse('instagram_home'))
 
-    context = RequestContext(request, {'request': request,'user': request.user})
+    context = RequestContext(request, {'request': request, 'user': request.user})
     return render_to_response('integrations/home.html', context_instance=context)
 
 
@@ -48,10 +45,12 @@ def get_texts(request):
     q = request.GET.get('term', '')
     return fuzzy_search_query(q, texts)
 
+
 def get_markov_texts(request):
     texts = TextMessageMarkov.objects.all()
     q = request.GET.get('term', '')
     return fuzzy_search_query(q, texts)
+
 
 def fuzzy_search_query(query, query_set):
     results = []
@@ -81,58 +80,53 @@ def twitter_home(request):
     template = loader.get_template('integrations/twitter_home.html')
     context = {'twitter_people': twitter_people}
     return HttpResponse(template.render(context, request))
-  
+
+
 def twitter_person_detail(request, person_username):
     author = None
     for person in TwitterPerson.objects.all():
         if person.username.strip() == person_username.strip():
             author = person
 
-    if not author:
-        for person in LiteraturePerson.objects.all():
-            if person.username.strip() == person_username.strip():
-                author = person
-
-    #If you are already on the page, these things 
-    #will happen when you click buttons
+    # If you are already on the page, these things
+    # will happen when you click buttons
     if(request.GET.get('go_back_to_list')):
         return HttpResponseRedirect(reverse('twitter_home'))
 
     if(request.GET.get('scrape')):
         scrape_twitter_person(author)
-        return HttpResponseRedirect('/integrations/twitter_person_detail/'+person_username)
+        return HttpResponseRedirect('/integrations/twitter_person_detail/' + person_username)
 
     if(request.GET.get('apply_markov_chains')):
         apply_markov_chains_twitter(author)
-        return HttpResponseRedirect('/integrations/twitter_person_detail/'+person_username)
+        return HttpResponseRedirect('/integrations/twitter_person_detail/' + person_username)
 
     template = loader.get_template('integrations/twitter_person_detail.html')
-        
+
     sentences = []
     markov_sentences = []
     person_type = ''
-    
-    #Grab data from author if twitter person    
+
+    # Grab data from author if twitter person
     if isinstance(author, TwitterPerson):
         person_type = str(TwitterPerson)
         twitter_sentences = TwitterPost.objects.filter(author=author)
-        sentences = [t.content for t in twitter_sentences] 
+        sentences = [t.content for t in twitter_sentences]
 
         twitter_markov_sentences = author.twitterpostmarkov_set.all().order_by('-randomness')
-        markov_sentences = \
-            [(t.content.encode('ascii', 'ignore'), \
-            t.randomness) for t in twitter_markov_sentences]
+        markov_sentences = [(t.content.encode('ascii', 'ignore'), t.randomness) for t in twitter_markov_sentences]
 
     context = RequestContext(request, {
-            'person': author,
-            'person_type': person_type,
-            'sentences' : sentences,
-            'markov_sentences'    : markov_sentences,
-            'len_sentences'    : len(sentences),
-            'len_markov_sentences'    : len(markov_sentences),
+        'person': author,
+        'person_type': person_type,
+        'sentences': sentences,
+        'markov_sentences': markov_sentences,
+        'len_sentences': len(sentences),
+        'len_markov_sentences': len(markov_sentences),
     })
 
     return HttpResponse(template.render(context))
+
 
 def instagram_home(request):
     """
@@ -140,7 +134,7 @@ def instagram_home(request):
     """
     if(request.GET.get('go_back_to_home')):
         return HttpResponseRedirect(reverse('home'))
-     
+
     if(request.GET.get('refresh_followers')):
         refresh_instagram_followers()
 
@@ -153,4 +147,3 @@ def instagram_home(request):
     me = get_me_from_instagram()
     context = {'num_posts': num_posts, 'num_followers': num_followers, 'me': me}
     return HttpResponse(template.render(context, request))
-

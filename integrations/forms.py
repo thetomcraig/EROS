f998498f998d8c34dc3e50b20e15a542a5db9314
Tom Craig <thetomcraig@icloud.com>
2016-11-21 21:05:47 -0800

    Working with form widgets...

--- integrations/forms.py
+++ integrations/forms.py
@@ -1,5 +1,22 @@
 from django import forms
+from .models import TwitterPerson
+
+
+TITLE_CHOICES = (
+    ('MR', 'Mr.'),
+    ('MRS', 'Mrs.'),
+    ('MS', 'Ms.'),
+)
 
 
 class PoolForm(forms.Form):
-    form_field1 = forms.CharField(max_length=40, required=True)
+
+    name = forms.CharField(max_length=100)
+    title = forms.CharField(
+        max_length=3,
+        widget=forms.Select(choices=TITLE_CHOICES),
+    )
+    birth_date = forms.DateField(required=False)
+
+    def init(self, choices):
+        self.choices = choices

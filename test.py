b4f5a234a4bff302a42f0fdd3c8580b82f00c63f
Tom Craig <thetomcraig@icloud.com>
2017-10-14 10:07:45 -0700

    Transitioning to python backend with react front end

--- test.py
+++
@@ -1,6 +0,0 @@
-import django
-django.setup()
-from integrations.helpers.twitter_utils import find_word_frequency_for_user
-
-find_word_frequency_for_user('justinbieber')
-find_word_frequency_for_user('Pink')
